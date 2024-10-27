d64994676abf293f5a9313457da45f0140df024c
Huy <dnmhuy@gmail.com>
2024-10-27 14:22:40 +0700

    hehehe

--- MilkyWay/Stuffs/static/javascript/MainPage.js
+++ MilkyWay/Stuffs/static/javascript/MainPage.js
@@ -111,12 +111,18 @@ function goToTop(){
    },1)
 }
 
-
-
-
-
-
-
 //run function
 CountDownTimer()
 goToTop()
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('input[name="slider"]');
+const numOfSlides = slides.length;
+
+function autoSlide() {
+    currentIndex = (currentIndex + 1) % numOfSlides;
+    slides[currentIndex].checked = true;
+}
+
+// Chuyển slide mỗi 5 giây
+setInterval(autoSlide, 3000);
