e20d170dbdd89255571cc3227606c3ebb59635b3
MinhKiet1102 <kiet7784@gmail.com>
2024-10-28 22:25:04 +0700

    database

--- MilkyWay/Staff/views.py
+++ MilkyWay/Staff/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render,redirect
 from Management.models import Users, Tour, Tickets, Booking, Payment, Review 
 from django.shortcuts import render, redirect
@@ -27,11 +26,8 @@ def get_payment(request):
 
 
 
-
 def get_add_tour(request):
-    tours = Tour.objects.all()
     if request.method == 'POST':
-        # Lấy dữ liệu từ request.POST và request.FILES
         user_tour = request.POST.get('tour_code')
         name = request.POST.get('tour_name')
         start_location = request.POST.get('start_location')
@@ -40,17 +36,17 @@ def get_add_tour(request):
         end_date = request.POST.get('end_date')
         price = request.POST.get('tour_price')
         available_seats = request.POST.get('passenger_count')
-        remaining_seats = available_seats  # Chỗ còn lại bằng chỗ trống ban đầu
+        remaining_seats = available_seats  
 
-        # Kết hợp các phần mô tả địa điểm, ngăn cách bằng dấu '*'
+        # Collect descriptions for each itinerary
         descriptions = []
         for i in range(1, 5):
             itinerary = request.POST.get(f'itinerary_{i}')
-            if itinerary:  # Chỉ thêm nếu có nội dung
+            if itinerary:
                 descriptions.append(itinerary)
-        description = '*'.join(descriptions)
+        description = ''.join(descriptions)  # Combine with '' separator
 
-        # Tạo một đối tượng Tour và lưu vào cơ sở dữ liệu
+        # Save Tour instance
         tour = Tour(
             user_tour=user_tour,
             name=name,
@@ -63,25 +59,18 @@ def get_add_tour(request):
             available_seats=available_seats,
             remaining_seats=remaining_seats,
         )
-        tour.save()  # Lưu tour vào cơ sở dữ liệu
+        tour.save()
 
-        # Lưu ảnh vào model Images
-        images = request.FILES.getlist('tour_image')  # Lấy tất cả các ảnh đã tải lên
+        # Save images to Images model
+        images = request.FILES.getlist('tour_image')
         for idx, image in enumerate(images):
-            image_instance = Images(
-                tour=tour,
-                images=image,
-                position=idx + 1  # Đánh số vị trí bắt đầu từ 1
-            )
-            image_instance.save()
+            Images.objects.create(tour=tour, images=image, position=idx + 1)
 
-        return redirect('tour_management')  # Chuyển hướng sau khi thêm thành công
+        return redirect('tour_management')
 
-    # Hiển thị trang thêm tour nếu là request GET
-
-    context = {'tour':tours,'range': range(1, 5)}
+    # Display add tour page if GET request
+    context = {'range': range(1, 5)}
     return render(request, 'tour_management/add_tour.html', context)
-
 
 def get_tour_edit(request):
     tour = Tour.objects.all()
