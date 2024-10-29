b1fd9ac6d0eeca3c98ad1744fa411d4b2126ad94
MinhKiet1102 <kiet7784@gmail.com>
2024-10-29 23:00:02 +0700

    commit

--- MilkyWay/Management/views.py
+++ MilkyWay/Management/views.py
@@ -6,7 +6,6 @@ from django.template import loader
 from Management.models import Users, Tour, Tickets, Booking, Payment, Review 
 
 def members(request):
-  template = loader.get_template('tour_list.html')
   tour = Tour.objects.all()
   user = Users.objects.all()
   ticket = Tickets.objects.all()
@@ -14,4 +13,73 @@ def members(request):
   payment = Payment.objects.all()
   review = Review.objects.all()
   context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
-  return HttpResponse(template.render(), context)
+  return render(request, 'admin_tour/tour_list.html', context)
+def create_group(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/create_group.html', context)
+
+def create_user(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/create_user.html', context)
+
+def group_management(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/group_management.html', context)
+
+def organized_tour(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/organized_tour.html', context)
+
+def tour_management(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/tour_management.html', context)
+
+def transaction_management(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/transaction_management.html', context)
+
+def user_management(request):
+  tour = Tour.objects.all()
+  user = Users.objects.all()
+  ticket = Tickets.objects.all()
+  booking = Booking.objects.all()
+  payment = Payment.objects.all()
+  review = Review.objects.all()
+  context = {'tour': tour, 'user':user, 'ticket':ticket, 'booking':booking, 'payment':payment, 'review':review}
+  return render(request, 'admin_tour/user_management.html', context)
