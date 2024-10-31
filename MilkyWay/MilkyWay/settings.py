b97ac0e5697b8ea76d4410d9c3336d3df07fc261
CongQuann <quanpt1610@gmail.com>
2024-10-31 18:29:40 +0700

    fix loi thang Hau

--- MilkyWay/MilkyWay/settings.py
+++ MilkyWay/MilkyWay/settings.py
@@ -158,5 +158,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ='/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Management/static/images')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
